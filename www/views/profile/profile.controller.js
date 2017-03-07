0bd3c960c2a0cffaeb1d2479a3e3008416be57db
inceon <mr.inceon@gmail.com>
2017-03-07 18:52:25 +0200

    added profile page

---
+++ www/views/profile/profile.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    angular
+        .module('app')
+        .controller('Profile', Profile);
+
+    Profile.$inject = ['$rootScope', '$state'];
+
+    function Profile($rootScope, $state) {
+
+        var vm = this;
+
+    }
+})();
