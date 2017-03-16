c06d1a9e4c80e2171955b7de1f34ae9a16c58d87
inceon <mr.inceon@gmail.com>
2017-03-16 15:27:12 +0200

    Added page for user rating

---
+++ www/views/user_rating/user_rating.controller.js
@@ -0,0 +1,18 @@
+/**
+ * Controller for user rating page
+ */
+(function () {
+    "use strict";
+
+    angular
+        .module('app')
+        .controller('UserRating', UserRating);
+
+    UserRating.$inject = ['$state', '$ionicHistory'];
+
+    function UserRating($state, $ionicHistory) {
+
+        var vm = this;
+
+    }
+})();
