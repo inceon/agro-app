f519fafc1d499596c21336801f74fe1becc63c0d
inceon <mr.inceon@gmail.com>
2017-03-30 17:51:04 +0300

    changed position text for more reading

--- www/directive/truncText.directive.js
+++ www/directive/truncText.directive.js
@@ -12,7 +12,7 @@
                 moreText: '@',
                 text: '='
             },
-            template: '<p class="text" ng-click="changeLimit()">{{text | limitTo: localLimit}}</p><p style="color: #ccc; text-align: center;" ng-click="changeLimit()" ng-show="text.length > localLimit && localLimit == limit"> {{moreText}} </p>',
+            template: '<p class="text" ng-click="changeLimit()">{{text | limitTo: localLimit}}</p><p style="color: #ccc; text-align: right;" ng-click="changeLimit()" ng-show="text.length > localLimit && localLimit == limit"> {{moreText}} </p>',
             link: function (scope) {
 
                 scope.localLimit = scope.limit;
